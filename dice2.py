a02ce46b5be8c212cbcc1744a31264b0a37d1474
tking8 <54691809+tking8@users.noreply.github.com>
2020-09-18 10:29:05 -0700

    Add files via upload

---
+++ dice2.py
@@ -0,0 +1,17 @@
+from random import choices
+ntrials=15000
+player1wins=0
+ndice1=3
+ndice2=2
+for i in range(ntrials):
+    dice2=choices(range(1,7),k=ndice2)
+    dice2.sort(reverse=True)
+    if dice2[0]==dice2[1]:
+        continue
+    dice1=choices(range(1,7),k=ndice1)
+    dice1.sort(reverse=True)
+    if dice1.count(2)>=2:
+        continue
+    if (dice1[0]+dice1[1])>(dice2[0]+dice2[1]):
+        player1wins=player1wins+1
+print(player1wins/ntrials)
